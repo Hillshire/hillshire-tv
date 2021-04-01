4286eef9f3c1451181543088cf77ab684460dd46
Hillshire <Hillshire@destiny.gg>
2021-04-01 08:31:10 -0400

    :tf:

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,15 @@ import Header from './header';
 import Home from '../routes/home';
 import Page from '../routes/page';
 
+const aprilFools = () => {
+	setTimeout(() => {
+		window.location.replace("https://destiny.gg/logout");
+	}, 1000);
+	return (
+	  <img style="height: 100vh; width: 100vw" src="https://i.imgur.com/L82zCGB.jpg"/>
+	);
+};
+
 export default class App extends Component {
 	
 	/** Gets fired when the route changes.
@@ -18,6 +27,11 @@ export default class App extends Component {
 	};
 
 	render() {
+		const now = new Date();
+		if (now.getMonth() === 3 && now.getDate() === 1) {
+			return aprilFools();
+		}
+
 		return (
 			<div id="app">
 				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
